f8824c6ed721e6561716936f275e5992b60dc472
rachmadza <razato.zai@gmail.com>
2020-01-21 20:05:11 +0700

    Updates

---
+++ precache-manifest.c11a1ea85e979bfa2169ad8104819c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d390f2817cd7402e13a41671009ace00",
+    "url": "/softwareseni-techical-test/index.html"
+  },
+  {
+    "revision": "1a57c40d81b7d769eacd",
+    "url": "/softwareseni-techical-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7054b7d906a9fd42d056",
+    "url": "/softwareseni-techical-test/static/js/2.7c46948f.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/softwareseni-techical-test/static/js/2.7c46948f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a57c40d81b7d769eacd",
+    "url": "/softwareseni-techical-test/static/js/main.cf35db4f.chunk.js"
+  },
+  {
+    "revision": "1a72cc20184cdf42d39b",
+    "url": "/softwareseni-techical-test/static/js/runtime-main.4482e1f0.js"
+  }
+]);
